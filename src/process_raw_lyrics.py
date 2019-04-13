c530fa9dfe79726136432841457d3eedcda2319a
Brian-Yeh <mrbyeh@gmail.com>
2019-04-12 22:31:13 -0400

    Update process_raw_lyrics.py to append to song_info.txt

--- src/process_raw_lyrics.py
+++ src/process_raw_lyrics.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import langid
-from textblob import TextBlob
-import time
+from tqdm import tqdm
+tqdm.pandas()
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_credentials_manager = SpotifyClientCredentials(client_id='***REMOVED***',
+                                                      client_secret='***REMOVED***')
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 bad_string = '          \r\n            Lyrics for this song have yet to be released. Please check back once the song has been released.\r\n          \r\n        '
 chunk_size = 200
 
 
 def get_language(lyrics):
-    # b = TextBlob(str(lyrics))
-    # return b.detect_language()
     return langid.classify(lyrics)[0]
 
 
@@ -20,31 +25,44 @@ def stats():
 
 
 def main():
-    lyrics_file = ''
-    raw = pd.read_csv(lyrics_file)
+    lyrics_file = 'lyrics_for_2019-4-5_14-17-22.txt'
+    raw = pd.read_csv('../data/lyrics/raw/'+lyrics_file)
     raw['lyrics'].astype(str)
-    raw.dropna(subset=['lyrics'], inplace=True)
 
+    raw.dropna(subset=['lyrics'], inplace=True)
     cleaned = raw[~raw.lyrics.str.contains(bad_string)]
     cleaned = cleaned[(cleaned['lyrics'] != '')
               & (cleaned['lyrics'] != 'This music does not contain words')
               & (cleaned['lyrics'] != 'Instrumental')
               & (cleaned['lyrics'] != '[Instrumental]')
               & (cleaned['lyrics'] != '[Non-Lyrical Vocals]')]
-    cleaned.to_csv(path_or_buf='../data/lyrics_cleaned.txt', index=False, encoding='utf-8')
+    cleaned.reset_index(drop=True, inplace=True)
 
-    # get lyrics language
-    cleaned = pd.read_csv('../data/lyrics_cleaned.txt', chunksize=chunk_size)
-    count = 0
-    write_header = True
 
-    for chunk in cleaned:
-        chunk['lang'] = chunk['lyrics'].apply(get_language)
-        chunk.to_csv(path_or_buf='../data/lyrics_with_lang.txt', mode='a',
-                     header=write_header, index=False, encoding='utf-8')
-        write_header = False
-        count += chunk_size
-        print('==== %s songs processed ====' % str(count))
+    cleaned['lang'] = cleaned['lyrics'].progress_apply(get_language)
+    cleaned.to_csv(path_or_buf='../data/lyrics/processed/1'+lyrics_file, header=True, index=False, encoding='utf-8')
+    # print(spotify.audio_features(cleaned.loc[0:50, 'song_id']))
+    # pbar.close()
+
+    # Get Audio features by chunks of 50
+    i = 0
+    chunk = 50
+    pbar = tqdm(total=len(cleaned))
+
+    while i < len(cleaned):
+        if (len(cleaned) - i) < 50:
+            chunk = len(cleaned) - i + 1
+        cleaned.loc[i:i + chunk, 'audio_features'] = spotify.audio_features(cleaned.loc[i:i + chunk, 'song_id'])
+        i += 50
+        pbar.update(50)
+    pbar.close()
+
+    # Add lyrics to song_info.txt
+    song_info = pd.read_csv('../data/song_info.txt')
+    song_info = pd.concat([song_info, cleaned], axis=0, sort=False, ignore_index=True)
+    song_info.drop_duplicates(subset='song_id', inplace=True)
+    song_info.to_csv(path_or_buf='../data/lyrics/song_info.txt', index=False, encoding='utf-8')
+
 
 
 main()
